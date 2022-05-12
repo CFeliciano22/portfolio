3f8381ba8b5a6f97d54a3875a6ec395361dfde81
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-05-12 03:01:42 -0400

    animation, styling and haver effects were added to the project.

--- src/components/Projects/ProjectItems.js
+++ src/components/Projects/ProjectItems.js
@@ -8,6 +8,7 @@ const ProjectItems =[
         src: require('../../assets/images/Tipsy.png'),
         github: 'https://github.com/cfeliciano22/cesar-feliciano-tipsy',
         website: '',
+        description: 'An application that allows users to search for restaurants with happy hours, full menu and reviews'
     },
     {
         id: 2,
@@ -15,6 +16,7 @@ const ProjectItems =[
         src: require('../../assets/images/Brainflix.png'),
         github: 'https://github.com/cfeliciano22/cesar-feliciano-brainflix',
         website: '',
+        description: 'Brainflix is a mock youtube SPA for streaming and uploading videos.'
     },
     {
         id: 3,
@@ -22,6 +24,7 @@ const ProjectItems =[
         src: require('../../assets/images/Bandsite.png'),
         github: 'https://github.com/cfeliciano22/cesar-feliciano-bandsite',
         website: '',
+        description: 'A website for a Band group with concert dates and information regarding the band.'
     },
     {
         id: 4,
@@ -29,6 +32,7 @@ const ProjectItems =[
         src: require('../../assets/images/Instock.png'),
         github: 'https://github.com/cfeliciano22/instock-api',
         website: '',
+        description: 'A collaborative group project made to manage stock take and inventory for different warehouses. Using jira and figma for design and layouts.'
     }
 ]
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,13 +14,18 @@ import 'swiper/css/pagination';
 
 export default function Projects() {
   return (
-    <section className="projects">
+    <section id="projects">
     <h3 className='projects__title'>Projects</h3>
         <div className='projects__container'>
             {ProjectItems.map((item) => {
                  return (
             <div className='projects__item'>
+                <div className='projects__item-image'>
                     <img key={item.id} className='image' src={item.src} alt={item.title}/>
+                    <div className='projects__image-overlay'>
+                        <p className='projects__description'>{item.description}</p>
+                    </div>
+                </div>
                     <h3 className='projects__item__title'>{item.title}</h3>
                     <a href={item.github} className='btn projects__btn' target='_blank'>Github</a>
                     <a href={item.website} className='btn projects__btn' target='_blank'>Website</a>
