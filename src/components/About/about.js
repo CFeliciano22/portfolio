8484fd3df6f79e6a799b6884cf6a24f3f240d5d0
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-06-01 23:43:01 -0400

    finalized the grammer on the about me

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -6,13 +6,13 @@ export default function About() {
             <h1 className='about__title'>About Me</h1>
             <div className='about__container'>
             <img className='about__image' src={pic} alt="me"/>
-            <p className='about__bio'>Hello, my name is Cesar Feliciano and im a recent gradute of Brainstation's 
+            <p className='about__bio'>Hello, my name is Cesar Feliciano and I'm a recent graduate of Brainstation's 
             Full-Stack Web Development Bootcamp (April 2022). I have a background 
             in Hospitality Management and Finance. With the recent changes coronavirus 
-            has brought us, I was able to explore a fascination I didnt think 
+            has brought us, I was able to explore a fascination I didn't think 
             I'd have. My passion for learning and solving problems was amplified,
             and has led me on the path of becoming a web developer. I am driven by
-            my passion and curiousity for tech, so in my free time I like to watch
+            my passion and curiosity for tech, so during my free time I like to watch
             insightful videos to sharpen my skills. I also enjoy exploring innovative 
             crypto projects that can have a positive impact not only in the finance world but 
             also the tech industry. I look forward to growing as a developer and contributing 
